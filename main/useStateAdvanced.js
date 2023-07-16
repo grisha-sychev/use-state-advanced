df0820a65b3fd6e190812190d18f3610704d6e19
Grigory Sychev <grisha_sychev@vk.com>
2023-07-16 14:02:48 +0300

    1.0.2

---
+++ main/useStateAdvanced.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+export default function useStateAdvanced(value) {
+    const [state, setState] = useState(value);
+
+    function updateState(path, value) {
+        setState(prevState => {
+            const newState = { ...prevState };
+            const keys = path.split('.');
+
+            let current = newState;
+            for (let i = 0; i < keys.length - 1; i++) {
+                const key = keys[i];
+                current[key] = current[key] || {};
+                current = current[key];
+            }
+
+            current[keys[keys.length - 1]] = value;
+            return newState;
+        });
+    }
+
+    return [state, updateState];
+}
